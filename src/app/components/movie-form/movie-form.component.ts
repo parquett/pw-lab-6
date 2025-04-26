194f0762effc5df32565bb653a3f9391c608b1e7
parquett <boss.leoneli@mail.ru>
2025-04-26 15:53:29 +0300

    add model and movie form component

---
+++ src/app/components/movie-form/movie-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/movie.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-movie-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './movie-form.component.html',
+  styleUrls: ['./movie-form.component.scss']
+})
+export class MovieFormComponent {
+  private _movieService = inject(MovieService);
+
+  protected movieForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    coverUrl: new FormControl('', [Validators.required]),
+    watchUrl: new FormControl('', [Validators.required])
+});;
+
+  addMovie(): void {
+    if (this.movieForm.valid) {
+      const newMovie: Movie = this.movieForm.value as Movie;
+      this._movieService.addMovie(newMovie);
+      this.movieForm.reset();
+    }
+  }
+} 
---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+  id?: number;
+  name: string;
+  coverUrl: string;
+  watchUrl: string;
+} 
