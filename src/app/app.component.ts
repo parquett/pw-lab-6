495ca67db0b81f58f92b826c0eca0aa2aaa71c69
parquett <boss.leoneli@mail.ru>
2025-04-26 16:39:29 +0300

    update index and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieFormComponent, MovieListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Lab6';
+  title = 'Movie Collection Manager';
 }
